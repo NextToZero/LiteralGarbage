88fca0a590b816182d2661c62c9c5a4c905a8575
Sayorecca <sayorecca@gmail.com>
2022-12-20 06:16:08 +0000

    Re-arranged JS

--- sometrash.js
+++ sometrash.js
@@ -1,3 +1,35 @@
+document.getElementById("garbage").onclick = function(){ colorcycle()}
+
+function colorcycle(){ 
+    if(document.getElementById("garbage").style.color == "red"){
+        document.getElementById("garbage").style.color = "blue"; 
+    }
+    else{
+        document.getElementById("garbage").style.color = "red" 
+    }
+
+
+}
+
+//the onclick function for the button
+function addTrash(){
+    //create a new paragraph
+    var newP = document.createElement("p");
+    //set the text of the paragraph to "more trash"
+    newP.innerHTML = "more trash";
+    //add the paragraph to the body
+    document.body.appendChild(newP);
+}
+
+//the onclick function for the addQuote button
+function addQuote(){
+    //create a new paragraph
+    var newP = document.createElement("p");
+    //set the text of the paragraph to the quote
+    newP.innerHTML = "The Dude abides.";
+    //add the paragraph to the body
+    document.body.appendChild(newP);
+}
 
 window.setInterval(function(){
     addElement();
