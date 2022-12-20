ba373e4dcd4d6e9cd14ddddb35ad377b1b4aafbb
NextToZero <j.chance.binder@gmail.com>
2022-12-19 19:11:57 -0800

    Purple

---
+++ sometrash.js
@@ -0,0 +1,24 @@
+
+window.setInterval(function(){
+    addElement();
+
+    
+}, 100);
+
+
+
+function addElement(){
+
+    const newDiv = document.createElement("div");
+
+    const newContent = document.createTextNode("LiTeRaL GaRbAgE");
+
+    newDiv.appendChild(newContent);
+
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+
+    
+
+
+}
